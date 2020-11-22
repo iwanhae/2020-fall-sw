ac3fc679c10b596f4bcc7776d2bafcf4d058541c
hyun gyu <gusrb3164@gmail.com>
2020-11-22 22:21:18 +0900

    body 데이터 변경사항 적용

--- back/processBody.py
+++ back/processBody.py
@@ -1,30 +1,30 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.parser import parse
-import statistics
 
 
 def processBody(body):
-    # 우선 data에 키 'value'가 없는 데이터를 찾음
-    list = []
-    checkValue = False
-    for i in body['data']:
-        if 'value' in i:
-            list.append(i['value'])
-        else:
-            checkValue = True
+    # date를 datetime형식으로 바꿔 줌
+    for i in body:
+        i['date'] = parse(i['date'])
 
-    median = statistics.median(list)  # 값이 있는 데이터만으로 중간값 추출
-    # 채워야할 데이터가 있을때만 실행하는 부분
-    if checkValue:
-        for idx, i in enumerate(body['data']):
-            if 'value' not in i:
-                body['data'][idx]['value'] = median
+    # date 기준 정렬
+    body.sort(key=lambda b: b['date'])
 
-    # date 형식이 2020-01-01 형식을 기준으로 오름차순으로 정렬함
-    body['data'] = sorted(
-        body['data'], key=lambda t: datetime.strptime(t['date'], '%Y-%m-%d'))
-    # 정렬된 데이터의 date를 datetime형식으로 바꿔 줌
-    for i in range(len(body['data'])):
-        body['data'][i]['date'] = parse(body['data'][i]['date'])
-    # 정렬되고 date가 datetime 형식인 body로 return
+    # 빈 데이터 날짜 사이값 직선 그래프 형태로 채우기
+    i = 0
+    while i < len(body)-1:
+        print(i)
+        x = (body[i+1]['date']-body[i]['date']).days
+        y = (body[i+1]['value']-body[i]['value'])
+        try:
+            grad = y/x  # 데이터간의 기울기
+        except:
+            grad = 1
+        idx = i
+        for j in range(1, x):  # 빈공간 메꾸는 루프
+            temp_date = body[i]['date']+timedelta(days=j)
+            temp_data = body[i]['value']+grad*j
+            body.insert(idx+1, {'date': temp_date, 'value': temp_data})
+            idx = idx+1
+        i = 1+idx
     return body
