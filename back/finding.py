0c5f9810908be8da938c99d3b0fdb73e2de8bebd
iwanhae <iwanhae@gmail.com>
2020-11-23 21:51:39 +0900

    백엔드 완성

--- back/finding.py
+++ back/finding.py
@@ -2,7 +2,33 @@ from db import getCol, getDB
 from datetime import datetime, timedelta
 import time
 
-def fetchKeywords(since : datetime, until : datetime) -> {}:
+# 1, 0, -1 로 변환된 두 데이터의 유사도를 비교하는 함수 클수록 유사도가 높음
+
+
+def compData(keyResult, userResult):
+    compResult = 0
+    for i in range(len(userResult)):
+        if keyResult[i] == userResult[i]:
+            compResult += 1
+    return compResult
+
+# 특정 키워드의 랭크 리스트값을 받아서 증감수치로만 변환해주는 함수
+
+
+def calVariance(data):
+    result = [0]
+    for i in range(1, len(data)):
+        if data[i] == data[i-1]:
+            result.append(0)
+        elif data[i] > data[i-1]:
+            result.append(1)
+        else:
+            result.append(-1)
+    return result
+
+
+def fetchKeywords(since: datetime, until: datetime) -> {}:
+    db = getDB()
     col = db.get_collection("ranked")
     data = {}
     length = (until - since).days + 1
@@ -20,11 +46,10 @@ def fetchKeywords(since : datetime, until : datetime) -> {}:
             if key not in data:
                 data[key] = [0 for i in range(length)]
             data[key][delta] = keyword['rank']
-        print(date)
-        clear_output(wait=True)
     return data
 
-def movingmean(d, size = 3):
+
+def movingmean(d, size=3):
     data = [0 for i in range(len(d))]
     for i in range(size):
         total = 0
@@ -38,6 +63,12 @@ def movingmean(d, size = 3):
         data[i] = total / size
     return data
 
+
+def sync(doc):
+    col = getCol()
+    return col.find_one_and_update({"_id": doc["_id"]}, {"$set": doc})
+
+
 def finding_related():
     col = getCol()
     print("finding related 시작")
@@ -47,33 +78,86 @@ def finding_related():
             print("[finding related] 찾은게 없어요")
             time.sleep(1)  # 작업할게 없으면 1초 뒤 다시확인
             continue
+
+        since = doc['meta']['from']
+        until = doc['meta']['to']
         doc["finished"] = False
-        doc["status"]["total"] = 0
+        doc["status"]["total"] = (until - since).days
         doc["status"]["current"] = 0
-        doc["status"]["message"] = "작업을 시작합니다."
-        since = doc["meta"]["from"]
-        until = doc["meta"]["to"]
-        tmp = fetchKeywords(since, until)
-        related_keys = []
-        for key in tmp:
-            mean1 = movingmean(tmp[key], 7) # db에 있는 키워드 주단위로 분석
-            mean2 = movingmean(doc["data"][0]["value"], 7) # 입력한 데이터 주단위로 분석
-            similarity = 1 - (compData(calVariance(mean2),calVariance(mean1)) / compData(calVariance(mean1),calVariance(mean1)))
-            related_keys.append((key, similarity))
-            doc["status"]["total"] = len(keywords)
+        doc["status"]["message"] = "데이터를 불러오는 중입니다."
+        sync(doc)
+        ########
+        # Fetch data
+        ########
+        db = getDB()
+        col_rank = db.get_collection("ranked")
+        keywords = {}
+        length = (until - since).days + 1
+        start = time.time()
+        for rank_doc in col_rank.find({
+            'status': 2,
+            'date': {
+                '$gte': since,
+                '$lte': until
+            }
+        }):
+            date = rank_doc['date']
+            delta = (date - since).days
+            for keyword in rank_doc['keywords']:
+                key = keyword['name']
+                if key not in keywords:
+                    keywords[key] = [0 for i in range(length)]
+                keywords[key][delta] = keyword['rank']
             doc["status"]["current"] += 1
-            if(doc["status"]["current"] % 50 == 0): #50개씩 작업되고 있다고 보여줌
-                col.find_one_and_update({"_id": doc["_id"]}, {"$set": doc})
-        related_keys.sort(key=lambda x:x[1]) #키워드 유사도 오름차순
-        for i in range(0, 20): #키워드 20개 뽑아서 저장
+            if time.time() - start > 1:
+                start = time.time()
+                sync(doc)
+        ########
+        # Data Conversion
+        ########
+        data_req = list(map(lambda x: x['value'], doc['data']))
+        ########
+        # Compare data
+        ########
+        related_keys = []
+        doc['status']['total'] = len(keywords)
+        doc['status']['current'] = 0
+        doc["status"]["message"] = "데이터를 비교해보는 중입니다."
+        sync(doc)
+        start = time.time()
+        for key in keywords:
+            data_cmp = movingmean(keywords[key], 7)  # db에 있는 키워드 주단위로 분석
+            similarity = 1 - (compData(calVariance(data_cmp),
+                                       calVariance(data_req)) / len(data_req))
+            related_keys.append((key, similarity))
+            doc["status"]["current"] += 1
+            if time.time() - start > 1:
+                start = time.time()
+                sync(doc)
+        ########
+        # Sort data
+        ########
+        doc['status']['total'] = 2
+        doc['status']['current'] = 0
+        doc["status"]["message"] = "거의 완료되었습니다."
+        doc["related"] = []
+        sync(doc)
+        related_keys.sort(key=lambda x: x[1])  # 키워드 유사도 오름차순
+        for i in range(0, 20):  # 키워드 20개 뽑아서 저장
+            tmp = []
+            d = doc['meta']['from']
+            for val in keywords[related_keys[i][0]]:
+                tmp.append({
+                    'date': d,
+                    'value': val
+                })
+                d += timedelta(1)
             doc["related"].append({
                 "keyword": related_keys[i][0],
                 "similarity": related_keys[i][1],
-                "data": [
-                    {
-                    "date": doc["data"][0]["date"],
-                    "value": doc["data"][0]["value"],
-                    }
-                ]
+                "data": tmp
             })
-        col.find_one_and_update({"_id": doc["_id"]}, {"$set": doc})
+        doc['status']['total'] = 2
+        doc['status']['current'] = 2
+        doc["status"]["message"] = "완료되었습니다. :-)"
+        sync(doc)
